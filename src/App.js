4a12c2ddf44027480975e8380e820dc448dfdccc
k.inoue <inoue@inouekeita.work>
2021-02-10 15:48:01 +0900

    開発環境構築

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from './Header';
+import Introduction from './Introduction';
+// import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Introduction />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Introduction.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Introduction extends React.Component {
+    render() {
+      return (
+          <main>
+            <p>Introductionです。</p>
+          </main>
+        )
+    }
+  }
+  export default Introduction;
